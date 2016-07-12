e3c7486184c856d48c1f7ca1efb7fca716bb7925
Craig Blaszczyk <masterjakul@gmail.com>
2016-07-12 15:25:48 +0100

    WIP add tests to make batch size a variabe

--- tests/base/tests.py
+++ tests/base/tests.py
@@ -1,6 +1,16 @@
 import os
+import unittest
 
+import troposphere
+
+from gordon.resources.base import BaseStream
+from gordon.utils import load_settings
 from gordon.utils_tests import BaseIntegrationTest, BaseBuildTest
+
+try:
+    from mock import patch, Mock
+except ImportError:
+    from unittest.mock import patch, Mock
 
 
 class IntegrationTest(BaseIntegrationTest):
@@ -18,3 +28,27 @@ class BuildTest(BaseBuildTest):
         self.assertBuild('0001_project', '0001_p.json')
         self.assertBuild('0001_project', '0002_pr_r.json')
         self.assertBuild('0001_project', '0003_r.json')
+
+
+class BatchSizeTest(unittest.TestCase):
+
+    def test_batch_size_read_from_settings(self):
+        settings = {
+            'lambda': '',
+            'stream': '',
+            'starting_position': '',
+            'batch_size': troposphere.Ref('BatchSize')
+        }
+        app = Mock(name='app')
+        app.name = 'app'
+        stream = BaseStream(
+            name='dummy', settings=settings, app=app, project=Mock()
+        )
+
+        self.assertEqual(stream.get_batch_size(), 123)
+
+    # def test_batch_size_minimum_respected(self):
+    #     raise NotImplementedError
+    #
+    # def test_batch_size_maximum_respected(self):
+    #     raise NotImplementedError
